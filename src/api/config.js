9447e671acd5e195e55c7402364b68f1c3b05c10
JOSE FONSECA MENDEZ <jose.fonseca.mendez@est.una.ac.cr>
2024-05-04 08:38:43 -0600

    baseURL fixed

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: 'https://proyectovoluntariados.azurewebsites.net/',
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://proyectovoluntariados.azurewebsites.net/',
+    // baseURL: 'http://localhost:3000/',
 
   });
 
